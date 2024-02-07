1158e104298e91938bd28ef222e4ccc75b00062a
Ajay <ajaykarthikeyan6@gmail.com>
2024-02-07 21:38:11 +0530

    Prime Number Checker

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Write ("Enter a number : ");
+int count = 0;
+if (int.TryParse (ReadLine (), out int num)) {
+   if (num == 1) WriteLine ("1 is neither prime nor composite");
+   else {
+      for (int i = 2; i <= num / 2; i++)
+         if (num % i == 0) {
+            count++;
+            break;
+         }
+      WriteLine ("It is a " + (count == 0 ? "Prime Number" : "not a Prime Number"));
+   }
+} else WriteLine ("Enter Valid Number");
