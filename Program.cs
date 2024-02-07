1917fc31dd2f8d504cca9210aaaea069c3aeb6c5
Ajay <ajaykarthikeyan6@gmail.com>
2024-02-07 21:41:30 +0530

    Palindrome Checker

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Program to check palindrome
+using static System.Console;
+Write ("Enter a string : ");
+var word = (ReadLine () ?? "").ToLower ().Replace (" ", "");
+if (word.Length != 0 && word.All (char.IsLetter)) {
+   var result = "It is a Palindrome";
+   for (int i = 0; i <= word.Length / 2 - 1; i++)
+      if (word[i] != word[word.Length - 1 - i]) {
+         result = "It is not a Palindrome";
+         break;
+      }
+   WriteLine (result);
+} else WriteLine ("Enter valid input");
