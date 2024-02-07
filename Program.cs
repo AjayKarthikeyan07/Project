c1b24194af74c588915895c5f87c5c58f1e37f37
Ajay <ajaykarthikeyan6@gmail.com>
2024-02-07 21:27:09 +0530

    Fi0bonacci series

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+var key = ConsoleKey.Y;
+while (key == ConsoleKey.Y) {
+   Write ("Enter number of terms for fibonacci series : ");
+   if (int.TryParse (ReadLine (), out int limit) && limit > 0)
+      Getfibonacci (limit);
+   else if (limit == 0 || limit < 0) WriteLine ("Insufficient terms for fibinocci series");
+   Write ("Do you want to get Fibonacci for Another Number (Y)es or (N)o : ");
+   key = ReadKey ().Key;
+   WriteLine ();
+}
+void Getfibonacci (int limit) {
+   int num1 = 0, num2 = 1, next, count = 2;
+   if (limit == 1) Write ($"{num1}");
+   else Write ($"{num1} {num2} ");
+   while (count < limit) {
+      next = num1 + num2;
+      (num1, num2) = (num2, next);
+      Write ($"{next} ");
+      count++;
+   }
+   WriteLine ();
+}
