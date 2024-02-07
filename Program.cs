db512978b3b80073e18e766ad5675b4507316b0c
Ajay <ajaykarthikeyan6@gmail.com>
2024-02-07 21:53:45 +0530

    Digital Root

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Digital root
+using static System.Console;
+Write ("Enter a Number : ");
+if (int.TryParse (ReadLine (), out int num) && num >= 0) {
+   while (num.ToString ().Length != 1) {
+      var digroot = num;
+      num = 0;
+      foreach (var ch in digroot.ToString ())
+         num += int.Parse (ch.ToString ());
+   }
+   WriteLine ($"Digital root : {num}");
+} else WriteLine ("Enter Valid Input");
