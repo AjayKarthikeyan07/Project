be3cace3eda9ffbedd29abba81ac66b03b0779b1
Ajay <ajaykarthikeyan6@gmail.com>
2024-02-07 22:16:24 +0530

    Factorial

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Program to Get Factorial of a Given Number
+using static System.Console;
+
+WriteLine ("Enter the number to calculate factorial : ");
+if (ulong.TryParse (ReadLine () ?? "", out ulong num))
+   WriteLine ($"The Factorial of {num} is : " + GetFactorial (num));
+else WriteLine ("Enter Valid Number");
+
+ulong GetFactorial (ulong n) {
+   if (n == 0) return 1;
+   if (n <= 2) return n;
+   return n * GetFactorial (n - 1);
+}
