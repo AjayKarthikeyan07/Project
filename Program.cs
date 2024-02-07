88b1fbb10c424abf525ef8aaa88124a085496d0f
Ajay <ajaykarthikeyan6@gmail.com>
2024-02-07 22:01:14 +0530

    Reduced String

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Program to remove Adjacent duplicate character
+using static System.Console;
+Write ("Enter a String : ");
+string letter = ReadLine () ?? "";
+for (int ch = 0; ch < letter.Length - 1; ch++)
+   for (int ch1 = ch + 1; ch1 < letter.Length;) {
+      if (letter[ch] == letter[ch1])
+         letter = letter.Remove (ch, 2);
+      else break;
+   }
+WriteLine ($"Modified String : {letter} ");
