ddc3870bb45e224dfb8a96b00c6070ab11cbf7a3
Ajay <ajaykarthikeyan6@gmail.com>
2024-02-07 21:49:52 +0530

    Reverse number and Palindrome check

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Reverse num and check for pallindrome
+using static System.Console;
+
+Write ("Enter a Number : ");
+if (int.TryParse (ReadLine (), out int num)) {
+   int orig = num;
+   int rev = 0;
+   while (num != 0) {
+      rev = (rev * 10) + num % 10;
+      num = num / 10;
+   }
+   WriteLine ("It is " + (orig == rev ? "a Palindrome" : "not a Palindrome"));
+} else WriteLine ("Enter valid Number");
