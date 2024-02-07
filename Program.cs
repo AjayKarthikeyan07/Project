865083cff781ae4e6774a82165ea3b6b928fb877
Ajay <ajaykarthikeyan6@gmail.com>
2024-02-07 22:11:59 +0530

    String Permutations

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Program to get all possible permutations of a string
+using static System.Console;
+
+string answer = "";
+Write ("Enter the string : ");
+int count = 0;
+string word = ReadLine () ?? "";
+List<string> permuteWords = new List<string> ();
+if (word.Length > 0 && word.All (char.IsLetter)) {
+   PrintPermute (word, answer);
+   Write ($"\nTotal Possible Permutations are {count} : \n");
+   foreach (var permute in permuteWords)
+      WriteLine (permute);
+} else WriteLine ("Enter Valid Input");
+
+void PrintPermute (string word, string answer) {
+   if (word.Length == 0) {
+      count++;
+      permuteWords.Add (answer);
+      return;
+   }
+   for (int i = 0; i < word.Length; i++)
+      PrintPermute (word[..i] + word[(i + 1)..], answer + word[i]);
+}
