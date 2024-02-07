c5688b3014f7cdb1d3abef5d2f4f59b5e75cdd8c
Ajay <ajaykarthikeyan6@gmail.com>
2024-02-07 22:21:38 +0530

    Swapping 2 Numbers

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Program to Swap 2 Numbers
+using static System.Console;
+
+WriteLine ("Enter the 2 numbers");
+if (!int.TryParse (ReadLine (), out int num1) || !int.TryParse (ReadLine (), out int num2))
+   WriteLine ("Enter Valid Number");
+else {
+   WriteLine ($"Original Numbers : num1 = {num1} num2 = {num2}");
+   PrintSwap (num1, num2);
+}
+
+void PrintSwap (int a, int b) {
+   (a, b) = (b, a);
+   WriteLine ($"Swapped Numbers  : num1 = {a} num2 = {b}");
+}
