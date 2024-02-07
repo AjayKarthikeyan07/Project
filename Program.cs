dfdaf263d185bf14540dd66193cd01623f3ef0bc
Ajay <ajaykarthikeyan6@gmail.com>
2024-02-07 21:47:20 +0530

    Print Diamond Pattern

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Diamond Pattern
+using static System.Console;
+Write ("Enter the height of the pattern : ");
+if (int.TryParse (Console.ReadLine () ?? "", out int height)) {
+   for (int i = 0; i <= height; i++) {
+      for (int blank = 0; blank < height - i; blank++)
+         Write (" ");
+      for (int j = 1; j <= (2 * i) - 1; j++)
+         Write ("*");
+      WriteLine ();
+   }
+   for (int i = height - 1; i > 0; i--) {
+      for (int blank = 0; blank < height - i; blank++)
+         Write (" ");
+      for (int j = 1; j <= (2 * i) - 1; j++)
+         Write ("*");
+      WriteLine ();
+   }
+} else WriteLine ("Enter valid Number");
