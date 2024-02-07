b9495d5215e2370d83fc4e5dd799ba8ef0b42082
Ajay <ajaykarthikeyan6@gmail.com>
2024-02-07 21:55:22 +0530

    Pascals's Training

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Pascal's Triangle
+using static System.Console;
+Write ("Enter number of rows : ");
+if (int.TryParse (ReadLine () ?? "", out int row)) {
+   int val = 1;
+   WriteLine ("Pascal's triangle");
+   for (int i = 0; i < row; i++) {
+      for (int blank = 1; blank <= row - i; blank++)
+         Write (" ");
+      for (int j = 0; j <= i; j++) {
+         if (j == 0 || i == 0 || j == i)
+            val = 1;
+         else
+            val = val * (i - j + 1) / j;
+         Write (val + " ");
+      }
+      WriteLine ();
+   }
+} else WriteLine ("Enter Valid Number");
