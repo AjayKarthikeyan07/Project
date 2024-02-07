39a8c3327a6ca16ad134af030ca5a6c59729c546
Ajay <ajaykarthikeyan6@gmail.com>
2024-02-07 21:45:40 +0530

    Multiplication Tables

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Tables
+using static System.Console;
+Write ("Till which tables do you want to print : ");
+if (int.TryParse (ReadLine () ?? "", out int count) && count > 0) {
+   int i = 1;
+   while (i <= count) {
+      for (int j = 1; j <= 10; j++)
+         WriteLine ($"{i} * {j,2} = {i * j,-1}");
+      i++;
+      WriteLine ();
+   }
+} else WriteLine ("Enter a valid Number");
