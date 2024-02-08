9bae6360bcde89784bc50a513e672dd4d15ec58c
Ajay <ajaykarthikeyan6@gmail.com>
2024-02-08 08:56:05 +0530

    Reverse Number guessing game (MSB to LSB)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//This Program makes the computer guess what number we think.
+using static System.Console;
+ForegroundColor = ConsoleColor.Cyan;
+int lowLimit = 0, highLimit = 100, baseLimit = (lowLimit + highLimit) / 2;
+//Setting lowlimit,highlimit and mid range
+WriteLine ("Heyy user think of a number between 1 to 100" + '\n' + "The computer will guess it ");
+ResetColor ();
+for (int tries = 1; tries <= 7; tries++) {
+   Write ($"Is the number greater than {baseLimit} (Y)es / (N)o ?  ");
+   switch (ReadKey ().Key) {
+      case ConsoleKey.Y:
+         lowLimit = baseLimit;
+         break;
+      case ConsoleKey.N:
+         highLimit = baseLimit;
+         break;
+      default:
+         WriteLine ("Enter valid number");
+         break;
+   }
+   //Reducing range after each iteration
+   baseLimit = (lowLimit + highLimit) / 2;
+   WriteLine ();
+}
+//Printing the guess
+WriteLine ();
+ForegroundColor = ConsoleColor.Green;
+WriteLine ($"The secret number is  {highLimit} ");
+ResetColor ();
