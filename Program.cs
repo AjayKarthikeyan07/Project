513da4fdfe0d3e1bddfc89061d2668a3e183b3a2
Ajay <ajaykarthikeyan6@gmail.com>
2024-02-08 09:08:51 +0530

    Spelling Bee Frequency

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string words = File.ReadAllText ("C:/etc/words.txt");
+Dictionary<char, int> freq = new ();
+for (char ch = 'A'; ch <= 'Z'; ch++) freq[ch] = 0;
+foreach (var ch in words)
+   if (ch is >= 'A' and <= 'Z')
+      freq[ch] = freq[ch] + 1;
+foreach (var item in freq.OrderByDescending (a => a.Value).Take (7))
+   Console.WriteLine (item);
