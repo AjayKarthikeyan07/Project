b789c39213fac5c71a6501aa357965e814831747
Ajay <ajaykarthikeyan6@gmail.com>
2024-02-08 08:54:02 +0530

    Number Guessing Game

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//This program is Number Guessing game!!
+using static System.Console;
+
+var game = ConsoleKey.Y;
+while (game == ConsoleKey.Y) {
+   //Variable to store random number generated
+   int secretnum = new Random ().Next (1, 101);
+   Write ("Guess the number between 1 to 100: ");
+   for (int tries = 1; ; tries++) {
+      string value = ReadLine () ?? "";
+      if (!int.TryParse (value, out int guess) || guess < 1 || guess > 100) {
+         // Getting valid integer input from user
+         WriteLine ("Enter valid integer within the range"); continue;
+      }
+      if (guess == secretnum) {  // check the guess
+         WriteLine ($"Your guess is correct. You took {tries} attempts."); break;
+      }
+      WriteLine ($"Your guess is too {(guess > secretnum ? "high" : "low")}");
+   }
+   // Asking user whether he wants to play again
+   Write ("Do you want to play again (Y)es / (N)o :");
+   game = ReadKey ().Key;
+   WriteLine ();
+}
