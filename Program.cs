9ca42955e5a78a082039f46779c6992cb830a864
Shakthy05 <shakthy2002@gmail.com>
2024-02-13 20:31:36 +0530

    Anagram

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ----------------------------------------------------------------------------------------
+// Program.cs
+// Anagram
+// To find anagrams from the given text file
+// ----------------------------------------------------------------------------------------
+
+using static System.Console;
+
+internal class Program {
+   private static void Main (string[] args) {
+      using (var stream = new StreamWriter ("D:/work/Anagrams.txt"))
+         // Sort each word in alphabetical order and group them
+         File.ReadAllLines ("D:/work/words.txt")
+             .GroupBy (word => string.Concat (word.OrderBy (ch => ch)))
+             .Where (word => word.Count () > 1)
+             .OrderByDescending (word => word.Count ())
+             // Transform the word into required format
+             .Select (word => $"{word.Count ()} {string.Join (", ", word)}")
+             .ToList ()
+             // Writing the output to console and file
+             .ForEach (word => { WriteLine (word); stream.WriteLine (word); });
+   }
+}
+
